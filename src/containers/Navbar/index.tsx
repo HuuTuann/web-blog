085cc778cc4b9c75f759ca8b95da0e3c5f28a803
Huu Tuan Nguyen <huutuan_nguyen@datahouse.com>
2025-04-22 23:28:43 +0700

    feat: add logout button to Navbar for user session management

--- src/containers/Navbar/index.tsx
+++ src/containers/Navbar/index.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Button } from "@/components";
 import { Paths } from "@/constants";
+import { removeCookie } from "@/services";
+import { LogOut } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useCallback } from "react";
 import { headerTitleMap } from "./helpers";
@@ -15,8 +18,11 @@ export const Navbar = () => {
   }, [pathname]);
 
   return (
-    <div className="flex h-[76px] w-full items-center border-b-2 border-slate-300 p-4">
+    <div className="flex h-[76px] w-full items-center justify-between border-b-2 border-slate-300 p-4">
       <h1 className="text-2xl font-bold text-slate-950">{getHeaderTitle()}</h1>
+      <Button variant="ioLight" isIconOnly onPress={removeCookie}>
+        <LogOut />
+      </Button>
     </div>
   );
 };
