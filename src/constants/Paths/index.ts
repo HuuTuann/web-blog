98a88a5bb1c55032c598189cc15ed152ce0cc0a3
Huu Tuan Nguyen <huutuan_nguyen@datahouse.com>
2025-04-29 20:41:21 +0700

    feat: Implement Management Blog, Business, and User modules with CRUD functionality

--- src/constants/Paths/index.ts
+++ src/constants/Paths/index.ts
@@ -6,4 +6,5 @@ export enum Paths {
   MANAGEMENT_USER = `${RootPaths.MANAGEMENT}/user`,
   MANAGEMENT_BLOG = `${RootPaths.MANAGEMENT}/blog`,
   MANAGEMENT_BUSINESS = `${RootPaths.MANAGEMENT}/business`,
+  MANAGEMENT_JOB = `${RootPaths.MANAGEMENT}/job`,
 }
--- src/containers/Navbar/helpers.ts
+++ src/containers/Navbar/helpers.ts
@@ -4,4 +4,5 @@ export const headerTitleMap = {
   [Paths.MANAGEMENT_USER]: "Management User",
   [Paths.MANAGEMENT_BLOG]: "Management Blog",
   [Paths.MANAGEMENT_BUSINESS]: "Management Business",
+  [Paths.MANAGEMENT_JOB]: "Management Job",
 } as const;
