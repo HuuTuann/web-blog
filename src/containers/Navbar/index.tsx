1ebef07467e5f7a34ed7004e532a6c90a6d3dccc
Huu Tuan Nguyen <huutuan_nguyen@datahouse.com>
2025-05-02 23:21:23 +0700

    feat: update Navbar component to include login state handling and conditional rendering of user profile and authentication buttons

--- src/containers/Navbar/index.tsx
+++ src/containers/Navbar/index.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { Button } from "@/components";
+import { Button, Link } from "@/components";
 import { Paths, RootPaths } from "@/constants";
 import { Toast } from "@/hooks";
-import { removeCookie } from "@/services";
+import { getCookie, removeCookie } from "@/services";
 import { LogOut } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useCallback } from "react";
 import UserProfile from "../User/UserProfile";
 import NavbarHelpers from "./helpers";
 
 export const Navbar = () => {
+  const router = useRouter();
   const pathname = usePathname();
 
   const getHeaderTitle = useCallback(() => {
@@ -25,16 +26,35 @@ export const Navbar = () => {
     window.open(`${process.env.NEXT_PUBLIC_WEB_URL}/login`, "_self");
   }, []);
 
+  const isLoggedIn = !!getCookie();
+
   const isUser = pathname.startsWith(RootPaths.USER);
 
   return (
     <div className="flex h-[76px] w-full items-center justify-between border-b-2 border-slate-300 p-4">
       <h1 className="text-2xl font-bold text-slate-950">{getHeaderTitle()}</h1>
       <div className="flex items-center gap-2">
-        {isUser && <UserProfile />}
-        <Button variant="ioLight" isIconOnly onPress={handleLogout}>
-          <LogOut />
-        </Button>
+        {isLoggedIn ? (
+          <>
+            {isUser && <UserProfile />}
+            <Button variant="ioLight" isIconOnly onPress={handleLogout}>
+              <LogOut />
+            </Button>
+          </>
+        ) : (
+          <>
+            <Link isBlock color="io" onPress={() => router.push("/register")}>
+              Register
+            </Link>
+            <Button
+              type="submit"
+              variant="ioSolid"
+              onPress={() => router.push("/login")}
+            >
+              Login
+            </Button>
+          </>
+        )}
       </div>
     </div>
   );
